03d92107d85405623c8ab92e075bfb61a3f84ba1
COValhalla <josephvellella@proton.me>
2022-10-21 11:17:04 -0600

    Sending json from API to front-end.

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,40 +2,40 @@ const Blogs = require('../models/Blogs');
 
 // Display list of all Blogs.
 exports.blog_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog list');
+  res.json({ message: 'NOT IMPLEMENTED: Blog list' });
 };
 
 // Display detail page for a specific Blog.
 exports.blog_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog detail: ' + req.params.id);
+  res.json({ message: 'NOT IMPLEMENTED: Blog detail: ' + req.params.id });
 };
 
 // Display Blog create form on GET.
 exports.blog_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog create GET');
+  res.json({ message: 'NOT IMPLEMENTED: Blog create GET' });
 };
 
 // Handle Blog create on POST.
 exports.blog_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog create POST');
+  res.json({ message: 'NOT IMPLEMENTED: Blog create POST' });
 };
 
 // Display Blog delete form on GET.
 exports.blog_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog delete GET');
+  res.json({ message: 'NOT IMPLEMENTED: Blog delete GET' });
 };
 
 // Handle Blog delete on POST.
 exports.blog_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog delete POST');
+  res.json({ message: 'NOT IMPLEMENTED: Blog delete POST' });
 };
 
 // Display Blog update form on GET.
 exports.blog_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog update GET');
+  res.json({ message: 'NOT IMPLEMENTED: Blog update GET' });
 };
 
 // Handle Blog update on POST.
 exports.blog_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Blog update POST');
+  res.json({ message: 'NOT IMPLEMENTED: Blog update POST' });
 };
--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,40 +2,40 @@ const Comments = require('../models/Comments');
 
 // Display list of all Comments.
 exports.comment_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment list');
+  res.json({ message: 'NOT IMPLEMENTED: Comment list' });
 };
 
 // Display detail page for a specific Comment.
 exports.comment_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment detail: ' + req.params.id);
+  res.json({ message: 'NOT IMPLEMENTED: Comment detail: ' + req.params.id });
 };
 
 // Display Comment create form on GET.
 exports.comment_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment create GET');
+  res.json({ message: 'NOT IMPLEMENTED: Comment create GET' });
 };
 
 // Handle Comment create on POST.
 exports.comment_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment create POST');
+  res.json({ message: 'NOT IMPLEMENTED: Comment create POST' });
 };
 
 // Display Comment delete form on GET.
 exports.comment_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment delete GET');
+  res.json({ message: 'NOT IMPLEMENTED: Comment delete GET' });
 };
 
 // Handle Comment delete on POST.
 exports.comment_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment delete POST');
+  res.json({ message: 'NOT IMPLEMENTED: Comment delete POST' });
 };
 
 // Display Comment update form on GET.
 exports.comment_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment update GET');
+  res.json({ message: 'NOT IMPLEMENTED: Comment update GET' });
 };
 
 // Handle Comment update on POST.
 exports.comment_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Comment update POST');
+  res.json({ message: 'NOT IMPLEMENTED: Comment update POST' });
 };
