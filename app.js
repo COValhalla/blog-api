21d2bc44292c3b74601827989520d5ed82ff4aba
COValhalla <josephvellella@proton.me>
2022-10-21 11:18:47 -0600

    Removed views. Using react for front-end.

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ var commentsRouter = require('./routes/comments');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
