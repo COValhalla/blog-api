1f54c0773bbf86ea705ef476261fbd12db0a6756
COValhalla <josephvellella@proton.me>
2022-10-24 09:41:52 -0600

    Added CORS for front-end connection.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,18 +12,21 @@ var commentsRouter = require('./routes/comments');
 
 var app = express();
 
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/blogs', blogsRouter);
-app.use('/blogs/:id/comments', commentsRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/blogs', blogsRouter);
+app.use('/api/blogs/:id/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
