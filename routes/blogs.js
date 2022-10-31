8d6eb0d68b72fce15ce44e99f94da0d9b8761891
COValhalla <josephvellella@proton.me>
2022-10-31 12:55:55 -0600

    Added more specific authentication.

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,24 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 const blog_controller = require('../controllers/blogController');
 
 // GET/POST request for creating a Blog.
-router.get('/create', blog_controller.blog_create_get);
-router.post('/create', blog_controller.blog_create_post);
+router.get(
+  '/create',
+  passport.authenticate('jwt', { session: false }),
+  blog_controller.blog_create_get,
+);
+router.post(
+  '/create',
+  passport.authenticate('jwt', { session: false }),
+  blog_controller.blog_create_post,
+);
 
 // GET/POST request to delete Blog.
-router.get('/:id/delete', blog_controller.blog_delete_get);
-router.delete('/:id/delete', blog_controller.blog_delete_post);
+router.get(
+  '/:id/delete',
+  passport.authenticate('jwt', { session: false }),
+  blog_controller.blog_delete_get,
+);
+router.delete(
+  '/:id/delete',
+  passport.authenticate('jwt', { session: false }),
+  blog_controller.blog_delete_post,
+);
 
 // GET/POST for updating Blog.
-router.get('/:id/update', blog_controller.blog_update_get);
-router.put('/:id/update', blog_controller.blog_update_put);
+router.get(
+  '/:id/update',
+  passport.authenticate('jwt', { session: false }),
+  blog_controller.blog_update_get,
+);
+router.put(
+  '/:id/update',
+  passport.authenticate('jwt', { session: false }),
+  blog_controller.blog_update_put,
+);
 
 // GET request for one Blog.
 router.get('/:id', blog_controller.blog_detail);
 
 // GET request for list of all Blog items.
+router.get('/', blog_controller.blog_list);
+
+// GET request for author's Blog items.
 router.get('/', blog_controller.blog_list);
 
 module.exports = router;
