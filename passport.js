31d67693d0f5177d45f3d38b9ec3cd426013816b
COValhalla <josephvellella@proton.me>
2022-11-01 11:08:20 -0600

    Reverting to original.

--- passport.js
+++ passport.js
@@ -7,11 +7,6 @@ const bcrypt = require('bcryptjs');
 const Users = require('./models/Users');
 require('dotenv').config();
 
-const options = {};
-
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = process.env.JWT_SECRET;
-
 // Passport
 passport.use(
   new LocalStrategy((username, password, done) => {
@@ -45,14 +40,20 @@ passport.deserializeUser(function (id, done) {
 });
 
 passport.use(
-  new JWTStrategy(options, function (jwtPayload, cb) {
-    //find the user in db if needed
-    return Users.findById(jwtPayload.id)
-      .then((user) => {
-        return cb(null, user);
-      })
-      .catch((err) => {
-        return cb(err);
-      });
-  }),
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    function (jwtPayload, cb) {
+      //find the user in db if needed
+      return Users.findById(jwtPayload.id)
+        .then((user) => {
+          return cb(null, user);
+        })
+        .catch((err) => {
+          return cb(err);
+        });
+    },
+  ),
 );
