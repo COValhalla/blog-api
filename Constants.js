867ec6e92eca25001aeb9b8b755d7d895428488b
COValhalla <josephvellella@proton.me>
2022-11-02 10:05:24 -0600

    Added readme. Changed API route.

--- Constants.js
+++ Constants.js
@@ -1,18 +1,14 @@
 const prod = {
   url: {
     API_URL: [
-      'https://blog-view-covalhalla.netlify.app/',
+      'https://blog-view-covalhalla.netlify.app',
       'http://localhost:5173',
     ],
   },
 };
 const dev = {
   url: {
-    API_URL: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://blog-view-covalhalla.netlify.app/',
-    ],
+    API_URL: ['http://localhost:5173', 'http://localhost:5174', ''],
   },
 };
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Blog-API (RESTful)
 
-A basic RESTful API for serving blogs and comments.
+A basic RESTful API for serving blogs and comments, hosted on Heroku.
 
 Root API: https://blog-api-covalhalla.herokuapp.com/
 
